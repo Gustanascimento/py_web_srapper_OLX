b9fb7cfedee68521c232fdcc429f826e6768d22e
Gustavo Nascimento <gustavoprazeres.nascimento@gmail.com>
2021-08-15 20:36:30 -0300

    Edit bot message

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,15 +7,18 @@ bot = telegram.Bot(token=token)
 def telegram_bot_send_message(titulo, data_postagem, preco, url):
     message = ''
     message += 'Novo anúncio detectado!'
-    message += '/n'
+    message += '\n'
     message += 'Título: '
     message += titulo
+    message += '\n'
     message += 'Data da postagem: '
     message += data_postagem
+    message += '\n'
     message += 'Preço: '
     message += preco
+    message += '\n'
     message += 'Link: '
     message += url
 
     bot.send_message(text=message, chat_id=chat_id)
-    print(f'telegram bot: message sent!')
+    print(f'Telegram Bot Says: "Message Sent!"')
